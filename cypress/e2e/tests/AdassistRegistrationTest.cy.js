c49d555667d74d9baeb3bc50fec3dfd57a6e3cd7
amitmehrotra100 <amit90@logiciel.io>
2024-09-30 19:32:42 +0530

    business profile registration

--- cypress/e2e/tests/AdassistRegistrationTest.cy.js
+++ cypress/e2e/tests/AdassistRegistrationTest.cy.js
@@ -25,15 +25,19 @@ describe("Adassist Sign-up Process", () => {
         cy.get(Adassist.BusinessProfileTxt).should('have.text', AdassistTestData.BusinessProfileTxt);
         cy.screenshot("Signup/SS05/full-page");
         const Business_Name = utilPage.generateRandomName(2,50);
-        cy.get(Signup.BusinessName)
+        cy.get(Adassist.BusinessName)
         .type(Business_Name)
         .should("be.visible")
         .and("have.value", Business_Name);
-        cy.get(Signup.NicheList).as('targetButton')  // Alias the button
+        cy.get(Adassist.NicheList).as('targetButton')  // Alias the button
         .should('be.visible'); // Ensure it's visible
-        cy.get('@targetButton').click({ multiple: true });
-        cy.get(Signup.NicheData).type(SignupTestData.NicheData);
-        cy.get(Signup.StreetAddress).type(`${SignupTestData.StreetAddressData}{enter}`).should('be.visible').get(Signup.StrreetAddressDropdown).first().click();
+        cy.get('@targetButton').eq(1).click();
+        cy.get(Adassist.NicheData).type(AdassistTestData.NicheData);
+        cy.get(Adassist.Nichesearchresult).click();
+        cy.get(Adassist.StreetAddress).type(`${AdassistTestData.StreetAddressData}{enter}`).should('be.visible').get(Adassist.StrreetAddressDropdown).first().click();
+        cy.get(Adassist.PhoneNumber).type(AdassistTestData.PhoneNumnber);
+        cy.get(Adassist.Continuebutton).eq(3).click();
+        
     })
 
 });
